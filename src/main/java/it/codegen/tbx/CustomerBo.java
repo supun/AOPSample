c8e804b6ed3846dd166d9e6ef46782a0dcf3f573
Supun dharmarathna <supun921@gmail.com>
2014-10-14 15:59:45 +0530

    Removed proprietary information

--- src/main/java/it/codegen/tbx/CustomerBo.java
+++ src/main/java/it/codegen/tbx/CustomerBo.java
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) 2014. CodeGen Ltd. - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- *
- * @author Supun Dharmarathne 
- * @since Oct 14, 2014
- */
+
 package it.codegen.tbx;
 
 public interface CustomerBo
